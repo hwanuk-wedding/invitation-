331a4bdb74b5c37d1675dd85027cd14091fec132
hwanuk-wedding <130133325+hwanuk-wedding@users.noreply.github.com>
2023-04-11 00:58:34 +0900

    230411
    파일 수정 후 업로드

---
+++ res/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+});
+
